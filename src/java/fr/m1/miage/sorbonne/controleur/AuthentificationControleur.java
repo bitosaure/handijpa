8252cfd3bc456c84845b545704abb02022a349a9
Emilie Pisu <emilie.pisu@gmail.com>
2017-05-08 14:40:45 +0200

    Integration de l’authentifiction

---
+++ src/java/fr/m1/miage/sorbonne/controleur/AuthentificationControleur.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.m1.miage.sorbonne.controleur;
+
+import fr.m1.miage.sorbonne.dao.PersonneDAO;
+import fr.m1.miage.sorbonne.entity.PersonneEntity;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author emiliepisu
+ 
+ */
+@ManagedBean
+@SessionScoped
+public class AuthentificationControleur implements Serializable{
+    private PersonneEntity      personne;
+
+    
+    private PersonneDAO   personneDao;
+
+    // Initialisation de l'entité utilisateur
+    public AuthentificationControleur() {
+        personne = new PersonneEntity();
+    }
+
+    // Méthode d'action appelée lors du clic sur le bouton du formulaire
+    // d'atuehtnification
+    public void authentification() {
+        System.out.println("je rentre");
+       
+    }
+
+    public PersonneEntity getPersonne() {
+        return personne;
+    }
+
+    
+}
