490ddec2ae668feff7af347d345f360702661ab2
Thibault Le Grand <thibaultlg@gmail.com>
2017-05-31 10:00:49 +0200

    Modif validator

--- src/java/fr/m1/miage/sorbonne/validator/NumberValidator.java
+++ src/java/fr/m1/miage/sorbonne/validator/NumberValidator.java
@@ -1,8 +1,12 @@
-package fr.ca.sa.aims.web.validators;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.m1.miage.sorbonne.validator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -10,22 +14,14 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-
 /**
- * Validateur de saisie de champs
- * 
- * @author LEGRATH - PISUEMI
  *
+ * @author thibault
  */
+@FacesValidator("numberValidator")
+public class NumberValidator implements Validator{
 
-@FacesValidator("champsValidator")
-public class ChampsValidator implements Validator {
-	/**
-	 * Méthode testant le contenu du champs grâce à une regexp
-	 * 
-	 */
-	@Override
-	public void validate(FacesContext arg0, UIComponent arg1, Object arg2)
+    public void validate(FacesContext arg0, UIComponent arg1, Object arg2)
 			throws ValidatorException {
 		
 		boolean valide = true;
@@ -33,17 +29,17 @@ public class ChampsValidator implements Validator {
 		if (chaine.isEmpty()) {
 			valide = true;
 		} else {
-			Pattern pattern = Pattern.compile("[a-zA-Z]+");
+			Pattern pattern = Pattern.compile("[0-9]+");
 			Matcher matcher = pattern.matcher(chaine);
 			valide = matcher.matches();
 		}
 		if (!valide) {
 			FacesMessage message = new FacesMessage(
 					FacesMessage.SEVERITY_ERROR,
-					"Le champs contient des chiffres","");
+					"Le champs contient des caractères alphanumériques","");
 			throw new ValidatorException(message);
 		}
 
 	}
-
+    
 }
